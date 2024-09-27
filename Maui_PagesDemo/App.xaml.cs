b87e6d8392744d8470f7b8287a2287420fa986e1
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2024-09-27 13:47:47 +0200

    Add project files.

---
+++ Maui_PagesDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui_PagesDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
